bd6463decb7805c1b51c303e286a02043b1e2f7a
Bujar Alimadhi <b.alimadhi@gmail.com>
2023-03-19 17:45:06 +0100

    fixing togle bug

--- src/component/photo/Photo.jsx
+++ src/component/photo/Photo.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./photo.css";
 
 function Photo({ src, isChecked, author, url }) {
-   return (
-      <div className="imageHolder">
-         <img src={src} alt="" className={`${isChecked ? "gray" : ""}`} />
-         <div className="caption">
-            <div>{author}</div>
-            <div>{url}</div>
-         </div>
+  return (
+    <div className="imageHolder">
+      <img src={src} alt="" className={isChecked ? "gray" : ""} />
+      <div className="caption">
+        <div>{author}</div>
+        <div>{url}</div>
       </div>
-   );
+    </div>
+  );
 }
 
 export default Photo;
