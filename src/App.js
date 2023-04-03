03005b63d938bec9eaea3a1789044f019b3a073d
Bujar Alimadhi <b.alimadhi@gmail.com>
2023-04-03 14:12:34 +0200

    adding gitHub link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
       <Gallery />
+      <p className="githubIcon">
+        Code on &nbsp;
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/alimadhibujar/Photo-Gallery"
+        >
+          <i className="fa fa-github" title="github"></i>
+        </a>
+      </p>
     </div>
   );
 }
